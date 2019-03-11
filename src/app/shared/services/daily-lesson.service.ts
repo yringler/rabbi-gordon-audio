910e410a361240ffde01a1404ca1f40832be7412
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-03-11 17:15:38 -0400

    cache to memory and file

--- src/app/shared/services/daily-lesson.service.ts
+++ src/app/shared/services/daily-lesson.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DailyStudyLibrary, DailyLessonTrack } from '../models/dailyLessons';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, from } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { knownFolders, Folder, File } from "tns-core-modules/file-system";
+import { stringify } from '@angular/core/src/util';
 
 const lessonApiUrl: string = 'lesson-api.herokuapp.com';
+
+function getFile(): File {
+	return knownFolders.documents().getFile("json");
+}
+
+function getFromFile(): DailyStudyLibrary {
+	try {
+		return JSON.parse(getFile().readTextSync());
+	}
+	catch {
+		return null;
+	}
+}
+
+function saveJson(json:string) {
+	getFile().removeSync();
+	
+	getFile().writeTextSync(json)
+}
 
 @Injectable({
 	providedIn: 'root'
@@ -13,7 +34,23 @@ export class DailyLessonServiceService {
 
 	constructor(private http: HttpClient) { }
 
+	library: DailyStudyLibrary;
+
 	getLibrary(): Observable<DailyStudyLibrary> {
+		// Try to load from memory.
+		if (this.library) {
+			return from([this.library]);
+		}
+
+		// Try to load from file.
+		let library = getFromFile();
+
+		if (library) {
+			this.library = library;
+			return this.getLibrary();
+		}
+
+		// Download, cache to memory and file.
 		return this.http.get<DailyLessonTrack[]>(lessonApiUrl).pipe(
 			map(tracks => {
 				let tracksWithDates = tracks.map(track => {
@@ -25,7 +62,10 @@ export class DailyLessonServiceService {
 				library.tracks = tracksWithDates;
 
 				return library;
-			})
+			}),
+			tap(library => this.library = library),
+			tap(library => saveJson(stringify(library)))
 		);
 	}
+	
 }
