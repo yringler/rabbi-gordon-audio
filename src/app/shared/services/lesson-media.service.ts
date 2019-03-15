1d63f8e297b8d1515fa32bd433feb5b5e64dadc3
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-03-15 13:57:03 -0400

    imporove

--- src/app/shared/services/lesson-media.service.ts
+++ src/app/shared/services/lesson-media.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { DailyLessonService } from './daily-lesson.service';
 import { LessonQuery, DailyLessonTrack} from '../models/dailyLessons';
 import { Observable } from 'rxjs';
-import { map, concat } from 'rxjs/operators';
+import { map, concatMap } from 'rxjs/operators';
 
 // Downloads media for given lessons, and saves file object to the lesson.
 function downloadMedia(lessons: DailyLessonTrack[]): Observable<DailyLessonTrack[]> {
@@ -22,7 +22,7 @@ export class LessonMediaService {
 	getFilesForLessons(query: LessonQuery) : Observable<DailyLessonTrack[]> {
 		return this.dailyLessonService.getLibrary().pipe(
 			map(library => library.query(query)),
-			concat(downloadMedia)
+			concatMap(tracks => downloadMedia(tracks))
 		)
 	}
 }
