9777907c409d06b5784df21956334c342125cce8
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-03-18 15:27:45 -0400

    fetch media

--- src/app/shared/services/lesson-media.service.ts
+++ src/app/shared/services/lesson-media.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@angular/core';
 import { DailyLessonService } from './daily-lesson.service';
 import { LessonQuery, DailyLessonTrack} from '../models/dailyLessons';
-import { Observable } from 'rxjs';
-import { map, concatMap } from 'rxjs/operators';
+import { Observable, from, zip } from 'rxjs';
+import { map, concatMap, tap } from 'rxjs/operators';
+import { getFile } from 'tns-core-modules/http/http';
+import { path, knownFolders, File } from 'tns-core-modules/file-system/file-system';
 
 // Downloads media for given lessons, and saves file object to the lesson.
-function downloadMedia(lessons: DailyLessonTrack[]): Observable<DailyLessonTrack[]> {
-	
+function downloadMedia(tracks: DailyLessonTrack[]): Observable<DailyLessonTrack[]> {
+	let observableArray: Observable<File>[] = [];
+
+	tracks.forEach(track => {
+		track.days.forEach(day => {
+			const filePath: string = path.join(knownFolders.currentApp().path, `${track.type}${day.date.valueOf()}`);
+			let fileLoad = from(getFile(day.source, filePath)).pipe(
+				tap(file => day.file = file)
+			)
+			observableArray.push(fileLoad);
+		});
+	});
+
+	return zip(...observableArray).pipe(
+		// After all the files are downloaded, re-emit the tracks array (which now have the file object set).
+		map(() => tracks)
+	)
 }
 
 @Injectable({
