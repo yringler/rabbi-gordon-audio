f6747fdae402180ff70f784d1218b254c6946123
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-03-19 13:24:26 -0400

    try switch to subject: WIP

--- src/app/shared/services/daily-lesson.service.ts
+++ src/app/shared/services/daily-lesson.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { getJSON } from "tns-core-modules/http";
 import { DailyStudyLibrary, DailyLessonTrack } from '../models/dailyLessons';
-import { Observable, from, concat } from 'rxjs';
-import { map, tap, catchError } from 'rxjs/operators';
+import { Observable, from, concat, ReplaySubject, BehaviorSubject } from 'rxjs';
+import { map, tap, catchError, multicast } from 'rxjs/operators';
 import { knownFolders, File, path } from 'tns-core-modules/file-system/file-system';
 
 const lessonApiUrl: string = 'https://lesson-api.herokuapp.com/';
@@ -33,47 +33,51 @@ function getFile(): File {
 export class DailyLessonService {
 	constructor() { }
 
-	private library: DailyStudyLibrary;
+	private library$: BehaviorSubject<DailyStudyLibrary>;
 
 	getLibrary(): Observable<DailyStudyLibrary> {
 		// Try to load from memory.
-		if (this.library) {
-			return from([this.library]);
+		if (this.library$) {
+			return this.library$;
 		}
 
-		// Try to load from file.
-		return from(getFile().readText()).pipe(
-			map(json => parseLibrary(json)),
-			// Make sure that file is up to date.
-			tap(library => {
-				if (!library.has({ date: 0 })) {
-					throw "Failed to load from file: doesn't have required dates."
-				}
-			}),
-			// If file loading fails, load from network.
-			catchError((e) => {
-				console.log(`Failed to load from file: ${e}`);
+		let libraryStream =
+			// Try to load from file.
+			from(getFile().readText()).pipe(
+				map(json => parseLibrary(json)),
+				// Make sure that file is up to date.
+				tap(library => {
+					if (!library.has({ date: 0 })) {
+						throw "Failed to load from file: doesn't have required dates."
+					}
+				}),
+				// If file loading fails, load from network.
+				catchError((e) => {
+					console.log(`Failed to load from file: ${e}`);
 
-				return from(getJSON<DailyLessonTrack[]>(lessonApiUrl)).pipe(
-					tap(tracks => ensureHasDates(<DailyLessonTrack[]>tracks)),
-					// Set the name where the media file will be saved.
-					tap(tracks => {
-						tracks.forEach(track => {
-							track.days.forEach(day => {
-								let fileName = path.join(knownFolders.documents().path, `${track.type}_${day.date.valueOf()}`);
-								day.file = fileName;
+					return from(getJSON<DailyLessonTrack[]>(lessonApiUrl)).pipe(
+						tap(tracks => ensureHasDates(<DailyLessonTrack[]>tracks)),
+						// Set the name where the media file will be saved.
+						tap(tracks => {
+							tracks.forEach(track => {
+								track.days.forEach(day => {
+									let fileName = path.join(knownFolders.documents().path, `${track.type}_${day.date.valueOf()}`);
+									day.file = fileName;
+								})
 							})
-						})
-					}),
-					// Save it to file.
-					tap(tracks => this.saveJson(JSON.stringify(tracks))),
-					// Convert it to a library.
-					map(tracks => new DailyStudyLibrary(tracks)),
-					// Save the library to memory.
-					tap(library => this.library = library)
-				)
-			})
-		)
+						}),
+						// Save it to file.
+						tap(tracks => this.saveJson(JSON.stringify(tracks))),
+						// Convert it to a library.
+						map(tracks => new DailyStudyLibrary(tracks))
+					)
+				})
+			);
+		
+		this.library$ = new BehaviorSubject(new DailyStudyLibrary([]));
+		libraryStream.subscribe(this.library$);
+
+		return this.library$;
 	}
 
 	private async saveJson(json: string) {
