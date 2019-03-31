b95efbe90da5a87d4a7e8767b1ad3ffb579e23df
Yehuda Ringler <ybringler@gmail.com>
2019-03-30 22:48:22 -0400

    purge files might work: dont delete too much

--- src/app/shared/services/allowed-files.service.ts
+++ src/app/shared/services/allowed-files.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { DailyLessonService } from './daily-lesson.service';
-import { File, path, knownFolders } from 'tns-core-modules/file-system/file-system';
+import { path, knownFolders } from 'tns-core-modules/file-system/file-system';
 import { map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
+// Files in the documents folder which should not be deleted.
+const protectedFiles = ["app", "internal", "metadata", "lessonJson"];
 
 @Injectable({
 	providedIn: 'root'
@@ -15,10 +18,12 @@ export class AllowedFilesService {
 		return this.lessonService.getLibrary().pipe(
 			// Get the lessons which are still current.
 			map(library => library.query({ date: -1, duration: 4 })),
-			// Get the file names.
-			map(tracks => new Array<string>().concat(...tracks.map(tracks => tracks.days.map(day => day.id)))),
-			// The lesson manifest is also allowed.
-			tap(fileNames => fileNames.push("lessonJson")),
+			// Map each track to array of file names.
+			map(tracks => tracks.map(tracks => tracks.days.map(day => day.id))),
+			// Flatten the 2 dimensional array.
+			map(tracks => new Array<string>().concat(...tracks)),
+			// Don't delete protected files.
+			tap(fileNames => fileNames.push(...protectedFiles)),
 			// Get full file paths.
 			map(fileNames => fileNames.map(name => path.join(knownFolders.documents().path, name)))
 		);
--- src/app/shared/services/purge-file.service.ts
+++ src/app/shared/services/purge-file.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AllowedFilesService } from './allowed-files.service';
 import { Observable, from, of } from 'rxjs';
-import { File, knownFolders } from 'tns-core-modules/file-system/file-system';
-import { zip, tap, mergeMap, map, catchError } from 'rxjs/operators';
-import { zip as staticZip } from 'rxjs';
+import { knownFolders } from 'tns-core-modules/file-system/file-system';
+import { mergeMap, map, catchError, tap } from 'rxjs/operators';
+import { zip } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root'
@@ -15,20 +15,22 @@ export class PurgeFileService {
 	purge(): Observable<string[]> {
 		let deletedFilePaths = new Array<string>();
 
-		return from(knownFolders.documents().getEntities()).pipe(
-			zip(this.allowedFiles.getAllowedFiles()),
+		return zip(
+			from(knownFolders.documents().getEntities()),
+			this.allowedFiles.getAllowedFiles()
+		).pipe(
 			// Delete files which aren't allowed.
 			mergeMap((([allFiles, allowedFiles]) => {
-				let deletingFiles = new Array<Observable<any>>();
+				let deletingFiles$ = new Array<Observable<any>>();
 				
 				allFiles.forEach(file => {
 					if (allowedFiles.indexOf(file.path) == -1) {
-						deletingFiles.push(from(file.remove()));
+						deletingFiles$.push(from(file.remove()));
 						deletedFilePaths.push(file.path);
 					}
 				});
 
-				return staticZip(...deletingFiles);
+				return zip(...deletingFiles$);
 			})),
 			map(() => deletedFilePaths),
 			catchError(error => {
