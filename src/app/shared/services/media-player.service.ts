6818ad3bd6227756533db4ac6d62772110f56d13
Yehuda Ringler <yehuda.ringler@chabad.org>
2019-05-03 13:03:28 -0400

    try no resume

--- src/app/shared/services/media-player.service.ts
+++ src/app/shared/services/media-player.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TNSPlayer } from 'nativescript-audio';
-import { File } from 'tns-core-modules/file-system/file-system';
+import { on, suspendEvent, ApplicationEventData } from 'tns-core-modules/application/application';
 
 @Injectable({
 	providedIn: 'root'
@@ -8,17 +8,41 @@ import { File } from 'tns-core-modules/file-system/file-system';
 export class MediaPlayerService {
 	private player: TNSPlayer;
 	private currentFile: string;
+	// #3: Bug in player: if app is moved to background and speaker is released, starts playing.
+	// Work around: on background, if not playing, stop, and save where stopped at to continue from there.
+	private stoppedAt: number;
 
 	constructor() {
 		this.player = new TNSPlayer();
+		
+		on(suspendEvent, () => {
+			// #3: When app is suspended, if paused, destroy player, and recreate, loading current file.
+
+			if (!this.player.isAudioPlaying() && this.currentFile) {
+				let stoppedAt = this.player.currentTime;
+
+				this.player.dispose().then(() => {
+					this.stoppedAt = stoppedAt;
+					this.player = new TNSPlayer();
+					this.player.initFromFile({
+						audioFile: this.currentFile,
+						loop: false
+					});
+				});
+			}
+		})
 	}
 
-	play(file: string) {
+	play(file: string, seek?: number) : Promise<any> {
 		this.currentFile = file;
 
-		this.player.playFromFile({
+		return this.player.playFromFile({
 			audioFile: file,
 			loop: false
+		}).then(() => {
+			if (seek) {
+				this.player.seekTo(seek);
+			}
 		})
 	}
 
@@ -28,7 +52,13 @@ export class MediaPlayerService {
 			this.player.pause();
 		} else {
 			if (this.currentFile != null && (requestedFile == null || this.currentFile == requestedFile)) {
-				this.player.resume();
+				// #3: If app is suspended, continue playing from the current place.
+				// When restored, reset stopped at property.
+				if (this.stoppedAt) {
+					this.play(this.currentFile, this.stoppedAt).then(() => this.stoppedAt = 0);
+				} else {
+					this.player.resume();
+				}
 			} else if (requestedFile != null) {
 				this.play(requestedFile);
 			} else {
